401f00df5eebc93e829720e5bcde337cff0d1093
Sven <skbotnl@disroot.org>
2025-05-27 13:09:23 +0200

    Change to more reliable death classification method

--- src/main/kotlin/net/trueog/questsOG/Events.kt
+++ src/main/kotlin/net/trueog/questsOG/Events.kt
@@ -1,6 +1,7 @@
 package net.trueog.questsOG
 
 import kotlinx.coroutines.DelicateCoroutinesApi
+import net.kyori.adventure.text.TranslatableComponent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
@@ -9,29 +10,26 @@ class Events : Listener {
     @OptIn(DelicateCoroutinesApi::class)
     @EventHandler
     fun onPlayerDeath(event: PlayerDeathEvent) {
-        if (event.deathMessage().toString().contains("death.fell.accident.water")) {
+        val translatableDeathMessage = event.deathMessage() as TranslatableComponent
+
+        if (translatableDeathMessage.key() == "death.fell.accident.water") {
             QuestsOG.redis.setValue("questsog:${event.player.uniqueId}:deaths:fellAccidentWater", "true")
             return
         }
 
-        if (event.deathMessage().toString().contains("fell while climbing")) {
+        if (translatableDeathMessage.key() == "death.fell.accident.other_climbable") {
             QuestsOG.redis.setValue("questsog:${event.player.uniqueId}:deaths:fellWhileClimbing", "true")
             return
         }
 
-        if (event.deathMessage().toString().contains("walked into the danger zone due to Zoglin")) {
+        val translatableDeathMessageArgs = translatableDeathMessage.args()
+
+        if (translatableDeathMessage.key() == "death.attack.hotFloor.player" && translatableDeathMessageArgs.size >= 2 && (translatableDeathMessageArgs[1] as TranslatableComponent).key() == "entity.minecraft.zoglin") {
             QuestsOG.redis.setValue(
                 "questsog:${event.player.uniqueId}:deaths:diedToMagmaBlockWhileFightingZoglin",
                 "true"
             )
             return
-        }
-
-        if (event.deathMessage().toString().contains("left the confines of this world while fighting Ender Dragon")) {
-            QuestsOG.redis.setValue(
-                "questsog:${event.player.uniqueId}:deaths:leftConfinesOfWorldWhileFightingEnderDragon",
-                "true"
-            )
         }
     }
 }
